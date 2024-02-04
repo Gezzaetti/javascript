2e7bb6969d56e57a37b43fc00397ab7340cf273d
Elias Hagos <eliasars@yahoo.com>
2024-02-04 15:07:42 +0200

    my message

---
+++ 04_week/04_exerices/basic_DOM/basic_DOM_1/script.js
@@ -0,0 +1,12 @@
+/* 
+
+Write JavaScript code that selects content id by using getElementById.
+Write a function which will update the text content of the selected element to "Hello, World!" by clicking the button.
+*/
+
+document.getElementById("myButton").addEventListener("click", function () {
+  alert("Hello, World!");
+});
+
+document.getElementById("content").textContent = "hello World";
+("Hello World");
